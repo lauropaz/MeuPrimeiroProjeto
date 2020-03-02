c5dd7f6a411c5a34c20907c7eaffd56fdad44323
leizapazcerimonial <lauro.paz@wdev.int>
2020-03-02 15:13:15 -0300

    primeiro commit

---
+++ src/test/java/com/lapaz/projeto/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lapaz.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
